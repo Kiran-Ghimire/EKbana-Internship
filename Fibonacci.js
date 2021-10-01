8736ccab6367c4b5f55633fd650ebc2959745fb6
N4secnT <54986538+N4secnT@users.noreply.github.com>
2021-10-01 10:24:18 +0545

    Fixed The Assignment Issues

--- Fibonacci.js
+++ Fibonacci.js
@@ -12,7 +12,7 @@ readline.question("Enter a positive number: ", (number) => {
   console.log("Fibonacci Series:");
 
   nextTerm = n1 + n2;
-  // console.log("FirstNextTerm", nextTerm);
+
   for (let i = 1; i <= number; i++) {
     console.log(n1);
     nextTerm = n1 + n2;
@@ -22,37 +22,3 @@ readline.question("Enter a positive number: ", (number) => {
 
   readline.close();
 });
-
-// function fib(n) {
-//   var a = 0,
-//     b = 1,
-//     c;
-//   if (n < 3) {
-//     if (n < 0) return fib(-n);
-//     if (n === 0) return 0;
-//     return 1;
-//   }
-//   while (--n) (c = a + b), (a = b), (b = c);
-//   return c;
-// }
-
-// console.log(fib(15));
-
-// const number = parseInt(prompt('Enter a positive number: '));
-// let n1 = 0, n2 = 1, nextTerm;
-// let count= 0;
-
-// console.log('Fibonacci Series:');
-
-// nextTerm = n1 + n2;
-
-// while (count < number) {
-
-//     // print the next term
-//     console.log(nextTerm);
-
-//     n1 = n2;
-//     n2 = nextTerm;
-//     nextTerm = n1 + n2;
-//     count +=1;
-// }
--- oddeven.js
+++ oddeven.js
@@ -4,34 +4,14 @@ const readline = require("readline").createInterface({
 });
 
 readline.question("Enter a number: ", (number) => {
-  // const result = number % 2 === 0 ? "even" : "odd";
   number = parseInt(number);
-  // console.log(`The number is ${result}.`);
-  // console.log(isNaN(number));
+
   if (isNaN(number)) {
     console.log("Please enter proper value");
   } else {
-    // if (number % 2 === 0) {
-    //   console.log("The number is even");
-    // } else {
-    //   console.log("The number is odd");
-    // }
     const result = number % 2 === 0 ? "even" : "odd";
     console.log(`The number is ${result}.`);
   }
 
   readline.close();
 });
-
-// function isOddOrEven(string) {
-//   if (typeof string !== "string") {
-//     return undefined;
-//   }
-//   if (string.length % 2 === 0) {
-//     return "even";
-//   }
-//   return "odd";
-// }
-
-// //console.log(isOddOrEven("opar"));
-// console.log(isOddOrEven("opa"));
